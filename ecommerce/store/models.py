6b4a40d344d1e09adcec91ced7eef0e8f815cd43
ilhamday <ilhamyudha00@gmail.com>
2020-08-03 14:50:06 +0700

    nambahin model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+# Generally, each model maps to a single database table. <- dari documentation
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200) #kayak buat kolom ditable, tipenya char
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True) # foreigen key, dari class Cutsomer
+    date_ordered = models.DateTimeField(auto_now_add=True) # tipenya date, auto nambah tanggal(datenya)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id) # id itu auto dibuatin sama django | karena angka, diconvert dulu jadi string
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True) #kalau SET_NULL harus diikutin null=True, kata mas verdyevan
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added =  models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
