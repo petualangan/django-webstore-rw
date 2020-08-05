46f65667b6ab7528707ec409db66ccd3d58f3bad
ilhamday <ilhamyudha00@gmail.com>
2020-08-05 11:16:08 +0700

    page checkout dinamis

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -5,14 +5,25 @@ from .models import * #import semua yang ada di models
 # Mendefinisikan halaman web
 
 def store(request):
-    products = Product.objects.all() #nyimpen semua isi dari Product yang ada dimodel ke -> products
+    products = Product.objects.all() #nyimpen semua isi dari Product yang ada dimodels ke -> products
     context = {
         'products': products
     }
     return render(request, 'store/store.html', context) # template store.html didalem folder store
 
 def cart(request):
-    context = {}
+
+    #kalau usernya login
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = [] #kalo nggak, itemnya kosong
+
+    context = {
+        'items': items
+    }
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
