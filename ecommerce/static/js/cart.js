bb22b4897fb161e52cee92cc35f143695a78bce1
ilhamday <ilhamyudha00@gmail.com>
2020-08-10 16:31:30 +0700

    csrf token, add to cart fungsional, angka di logo keranjang

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -4,6 +4,36 @@ for(i = 0; i < updateBtns.length; i++) {
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
-        console.log('productId:',productId, 'Action:',action)
+        console.log('productId:',productId, 'Action:',action) // diconsole nanti, muncul id sama actionnya | action -> store.html -> data-action="add"
+
+        console.log('USER:', user)
+        if (user == 'AnonymousUser'){ //kalau user belum login
+            console.log('User is not authenticated')
+        }else{ // kalau user udah login
+            updateUserOrder(productId, action)
+        }
     })
+}
+
+function updateUserOrder(productId, action){
+    console.log('User is authenticated, sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken, // csrftoken dapet dari main.html
+        },
+        body:JSON.stringify({'productId':productId, 'action':action})
+    })
+        .then((response) => { // ini promise. masih belum tau itu apaan
+            return response.json();
+        })
+         .then((data) => {
+             console.log('Data:', data)
+             location.reload() // update angka di logo keranjang
+        });
+
 }
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from .models import * #import semua yang ada di models
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 # Mendefinisikan halaman web
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []  # kalo nggak, itemnya kosong
+        order = {'get_cart_total': 0,'get_cart_items': 0}  # percobaan kalau logout (ngga ada user login di localhost/admin), efeknya halaman cart akan kosong
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all() #nyimpen semua isi dari Product yang ada dimodels ke -> products
+
     context = {
-        'products': products
+        'products': products,
+        'cartItems': cartItems
     }
+
     return render(request, 'store/store.html', context) # template store.html didalem folder store
 
 def cart(request):
@@ -17,13 +32,16 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = [] #kalo nggak, itemnya kosong
         order = {'get_cart_total': 0, 'get_cart_items': 0} #percobaan kalau logout (ngga ada user login di localhost/admin), efeknya halaman cart akan kosong
+        cartItems = order['get_cart_items']
 
     context = {
         'items': items,
         'order': order,
+        'cartItems': cartItems
     }
     return render(request, 'store/cart.html', context)
 
@@ -32,12 +50,41 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0} #percobaan kalau logout (ngga ada user login di localhost/admin), efeknya halaman cart akan kosong
+        cartItems = order['get_cart_items']
 
     context = {
         'items': items,
         'order': order,
+        'cartItems': cartItems
     }
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body) # bodynya yang ada di cart.js
+    productId = data['productId']
+    action = data['action'] # logicnya udah di buat di cart.js
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False) # kalo tombol add to cart ditekan, akan menghubungkan dari order ke customer
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product) # menghubungkan orderItem ke variable order(isinya order) diline atas, begitu juga product
+
+    # membuat quantity di cart.html bekerja, BUKAN tombol panah naek/turun-nya
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False) # memberikan return berupa Json
